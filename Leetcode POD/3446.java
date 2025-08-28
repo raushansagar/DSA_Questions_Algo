1af974e1e0dc41631d7a2d4b68a9cd45675f8f9e
RaushanSagar1694 <dayasagar1694@gmail.com>
2025-08-29 00:45:15 +0530

    pod

--- Leetcode POD/3446.java
+++ Leetcode POD/3446.java
@@ -1,4 +1,6 @@
 
+
+// approch (1)
 // time complexity of code is o(n*m * log Min(n*m))
 import java.util.*;
 
@@ -38,6 +40,52 @@ class Solution {
                     grid[i][j] = map.get(key).pollFirst();
                 }
             }
+        }
+
+
+        return grid;
+    }
+}
+
+
+
+// approch (2)
+// time complexity is 0(n*n log(Min n*m))
+
+class Solution2 {
+    public void solve(int i, int j, int[][] grid, List<Integer> list, int n, int m){
+        if(i < 0 || i >= n || j < 0 || j >= m){
+            if((i-j) < 0){
+                Collections.sort(list);
+            }
+            else{
+                Collections.sort(list, Collections.reverseOrder());
+            }
+            return;
+        }
+
+        list.add(grid[i][j]);
+        solve(i+1, j+1, grid, list, n, m);
+
+        // backtrack
+        grid[i][j] = list.get(list.size()-1);
+        list.remove(list.size()-1);
+    }
+    public int[][] sortMatrix(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        
+
+        // increasing order
+        for(int i = 1; i < n; i++){
+            List<Integer> list = new ArrayList<>();
+            solve(0, i, grid, list, n, m);
+        }
+
+        // decreasing order
+        for(int i = 0; i < n; i++){
+            List<Integer> list = new ArrayList<>();
+            solve(i, 0, grid, list, n, m);
         }
 
 
